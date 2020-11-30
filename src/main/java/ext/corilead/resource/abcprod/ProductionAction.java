f08fea9bc39757f9f1173b1f1979dbf6bddeaabc
王皓 <haowang@corilead.com>
2020-11-30 18:00:03 +0800

    logs

---
+++ src/main/java/ext/corilead/resource/abcprod/ProductionAction.java
@@ -0,0 +1,18 @@
+package ext.corilead.resource.abcprod;
+
+import wt.util.resource.RBEntry;
+import wt.util.resource.RBUUID;
+import wt.util.resource.WTListResourceBundle;
+
+@RBUUID("ext.corilead.resource.abcprod.ProductionAction")
+public class ProductionAction  extends WTListResourceBundle {
+
+    @RBEntry("创建投产单")
+    public final static String test_1 = "ProductionSingle.ProductionCreate.description";
+    @RBEntry("创建投产单")
+    public final static String test_2 = "ProductionSingle.ProductionCreate.title";
+    @RBEntry("创建投产单")
+    public final static String test_3 = "ProductionSingle.ProductionCreate.tooltip";
+    @RBEntry("accsstl.gif")
+    public final static String test_4 = "ProductionSingle.ProductionCreate.icon";
+}
